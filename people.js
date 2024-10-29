7e7869e6adaaad0fcf9782959c09dc29559b01c7
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-29 16:25:46 +0100

    final function

--- people.js
+++ people.js
@@ -1,10 +1,20 @@
-const  getName = require("./names");
+const getName = require("./names");
 const getHobbies = require("./hobbies");
 
 
-console.log(getName("Francesco", "Marra"));
-console.log(getHobbies("Calcio", "Carte", "Videogiochi"));
+/* console.log(getName("Francesco", "Marra"));
+console.log(getHobbies("Calcio", "Carte", "Videogiochi")); */
 
 
+function createPerson() {
+    const names = getName("Francesco", "Marra");
+    const hobby = getHobbies("Calcio", "Carte", "Videogiochi")
 
+    return {
+        firstName: names.firstName,
+        lastName: names.lastName,
+        hobbies: hobby.hobbies
+    }
+}
+console.log(createPerson());
 
